dc35faafddc3c43ab640beea8da25d945b0f07d3
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2023-01-19 17:57:02 -0300

    Criação de uma tela de login

--- Jogoteca.py
+++ Jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogos():
     def __init__(self, nome, genero, console):
@@ -7,10 +7,11 @@ class Jogos():
         self.console = console
 
 app = Flask(__name__)
+app.secret_key = 'helloworld'
 
 @app.route('/')
 def inicio():
-    return render_template('lista_jogos.html', titulo="Jogos", jogos = lista)
+    return render_template('index.html', titulo="Jogos", jogos = lista)
 
 jogo1 = Jogos('Fortnite', 'Battle Royale', 'PC')
 jogo2 = Jogos('Spider-Man PS4', 'Ação', 'PS4')
@@ -21,7 +22,7 @@ lista = [jogo1, jogo2, jogo3]
 def novo():
     return render_template('novo.html', titulo="Novo Jogo")
 
-@app.route('/criar', methods= ['POST',])
+@app.route('/criar', methods = ['POST',])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
@@ -29,5 +30,19 @@ def criar():
     jogo = Jogos(nome, categoria, console)
     lista.append(jogo)
     return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods = ['POST',])
+def autenticar():
+    if '58214970' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(f'Usuário {session["usuario_logado"]} logado com sucesso!')
+        return redirect('/')
+    else:
+        flash("Usuário não logado!")
+        return redirect('/login')
 
 app.run(debug=True)
