1cc27f2a315535fe636f8d9a55d262e4d881b523
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2023-01-19 18:06:50 -0300

    Adição de uma nova rota para logout

--- Jogoteca.py
+++ Jogoteca.py
@@ -45,4 +45,10 @@ def autenticar():
         flash("Usuário não logado!")
         return redirect('/login')
 
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash("Usuário deslogado com sucesso!")
+    return redirect('/')
+
 app.run(debug=True)
