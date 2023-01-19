ade5db35cc68e396aad63411e2eb406a0ade254c
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2023-01-19 15:44:22 -0300

    Adição de uma nova página que permite cadastrar novos jogos e envia as informações para a lista. As páginas foram estilizadas com Bootstrap.

--- Jogoteca.py
+++ Jogoteca.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+class Jogos():
+    def __init__(self, nome, genero, console):
+        self.nome = nome
+        self.genero = genero
+        self.console = console
 
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-    return render_template('lista_jogos.html')
+@app.route('/')
+def inicio():
+    return render_template('lista_jogos.html', titulo="Jogos", jogos = lista)
 
-app.run()
+jogo1 = Jogos('Fortnite', 'Battle Royale', 'PC')
+jogo2 = Jogos('Spider-Man PS4', 'Ação', 'PS4')
+jogo3 = Jogos('God of War', 'Ação', 'PS4')
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo="Novo Jogo")
+
+@app.route('/criar', methods= ['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogos(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+app.run(debug=True)
